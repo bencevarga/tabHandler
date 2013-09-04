aa1ffa0276efa070317d3359b83a6788ea5be879
Bence Varga <vargabence1@gmail.com>
2013-09-04 13:11:56 -0700

    Drag and drop feature for the list elements.

--- development/js/tabHandler.js
+++ development/js/tabHandler.js
@@ -5,78 +5,65 @@ var tabHandler;
 	var tabsContainer;
 	var groupId = 0;
 	var tabGroups = {
-		
+
 	};
+	_grabbedRow = null;
 	TabHandler.prototype = {
 		init : function() {
 
-			chrome.windows
-					.getCurrent(
-							{
-								populate : true
-							},
-							function(window) {
-								
-								jQuery('#addCategory').click({
-								}, function() {
-									addCategory();
-								}).disableSelection();
-								tabsContainer = jQuery("#tabsContainer");
+			chrome.windows.getCurrent({populate : true},
+				function(window) {
+	
+					jQuery('#addCategory').click({}, function() {
+						addCategory();
+					}).disableSelection();
+					tabsContainer = jQuery("#tabsContainer");
+	
+					var tabsList = addCategory();
+	
+					var row = 0;
+					var defaultTabGroup = tabGroups[0];
+	
+					for (key in window.tabs) {
+						if (window.tabs.hasOwnProperty(key)
+								&& window.tabs[key].title != "TabHandler") {
+							row++;
+	
+							defaultTabGroup.tabs[window.tabs[key].id] = createTabListElementContent(window.tabs[key]);
+	
+							defaultTabGroup.tabs[window.tabs[key].id].htmlElements.li
+									.appendTo(tabsList);
+	
+							// chrome.tabs.remove(window.tabs[key].id);
+						}
+					}
+					console.info("groups:", tabGroups);
+				});
 
-								var tabsList = addCategory();
-
-								var row = 0;
-								var defaultTabGroup = tabGroups[0];
-								
-//								tabGroups[defaultTabGroup.id] = defaultTabGroup;
-								for (key in window.tabs) {
-									if (window.tabs.hasOwnProperty(key)
-											&& window.tabs[key].title != "TabHandler") {
-										row++;
-
-										defaultTabGroup.tabs[window.tabs[key].id] = createTabListElementContent(window.tabs[key]);
-		
-										defaultTabGroup.tabs[window.tabs[key].id].htmlElements.li.appendTo(tabsList);
-										
-										// chrome.tabs.remove(window.tabs[key].id);
-									}
-								}
-								console.info("groups:",tabGroups);
-							});
-
-			addEventListeners();
+				addEventListeners();
 
 		},
-		getGroups : function(){
+		getGroups : function() {
 			return tabGroups;
 		}
 	};
 
 	function createTabListElementContent(tab, groupId) {
 
-		var link = jQuery("<a href='' class='tabTitle'>" + tab.title + "</a>");
+		var link = jQuery("<a href=''><span class='tabTitle'>" + tab.title + "</span></a>");
 
 		var settings = jQuery('<div class="settings"></div>').appendTo(link);
-		settings.append("<div class='handler'></div>");
+		var dropDownHandler = jQuery("<div class='handler'></div>");
+		settings.append(dropDownHandler);
 
 		var url = jQuery('<span class="tabListUrl">' + tab.url + '</span>');
 		url.appendTo(link);
 
-		link.click({
-			tab : tab
-		}, function(e) {
-			e.preventDefault();
-			console.info("tab that is opened: {}", e.data.tab);
-			chrome.tabs.highlight({
-				tabs : [ e.data.tab.index ]
-			}, function() {
-			});
-
-		});
+		link.click({tab : tab}, highlightTab);
 
 		var favicon = jQuery("<img class='favicon' src='" + tab.favIconUrl
 				+ "'/>");
-		
+
 		var tabListElement = new TabGroupElement({
 			tab : tab,
 			htmlElements : {
@@ -88,29 +75,84 @@ var tabHandler;
 				url : url
 			}
 		});
-		
+
+		dropDownHandler.mousedown({
+			tabListElement : tabListElement
+		}, function(ev) {
+			_grabbedRow = {
+				tabListElement : ev.data.tabListElement,
+				group : getGroupByTab(tab)
+			}
+		});
+
 		return tabListElement;
+	}
+	function highlightTab(event) {
+		e.preventDefault();
+		chrome.tabs.highlight({
+			tabs : [ e.data.tab.index ]
+		}, function() {
+		});
 	}
 	function addCategory() {
 		var tabGroup = jQuery('<div class="tabGroup"></div>').appendTo(
 				tabsContainer);
-		var tabsList = jQuery('<ol  class="tabList"><ol>').sortable({
-			connectWith : ".tabList",
-			handle : ".handler",
-			dropOnEmpty : true
-		}).disableSelection().appendTo(tabGroup);
-		
-		
-		tabGroups[groupId] = new TabGroup(groupId++,tabsList);
+
+		var tabsList = jQuery('<ol id=list' + groupId + ' class="tabList"><ol>')
+				.sortable({
+					connectWith : ".tabList",
+					handle : ".handler",
+					dropOnEmpty : true,
+					stop : handleDropEvent
+				}).disableSelection().appendTo(tabGroup);
+
+		tabGroups[groupId] = new TabGroup(groupId++, tabsList);
+
 		return tabsList;
 
-		
-		
+	}
+	function handleDropEvent(event, ui) {
+		if (_grabbedRow != null) {
+			var newGroup = getGroupByElement(ui.item.parent());
+			console.info("new group is ", newGroup);
+			delete _grabbedRow.group.tabs[_grabbedRow.tabListElement.tab.id];
+			newGroup.tabs[_grabbedRow.tabListElement.tab.id] = _grabbedRow.tabListElement;
+
+			_grabbedRow = null;
+		}
 	}
 	function addEventListeners() {
-		chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-			console.info("changed ", changeInfo.url);
-		});
+		chrome.tabs.onUpdated.addListener(refreshListElement);
+	}
+	function refreshListElement(tabId, changeInfo, tab){
+		var parentGroup = getGroupByTab(tab);
+		var updatedTablistElement = parentGroup.tabs[tab.id];
+		console.info("updatedTablistElement: ",updatedTablistElement.htmlElements.url.value(""));
+		
+	}
+	function getGroupByTab(tab) {
+		for ( var key in tabGroups) {
+			if (tabGroups.hasOwnProperty(key)) {
+				for ( var tabKey in tabGroups[key].tabs) {
+					if (tabGroups[key].tabs.hasOwnProperty(tabKey)) {
+						if (tab.id == tabGroups[key].tabs[tabKey].tab.id) {
+							return tabGroups[key];
+						}
+					}
+				}
+			}
+		}
+		return null;
+	}
+	function getGroupByElement(element) {
+		for ( var key in tabGroups) {
+			if (tabGroups.hasOwnProperty(key)) {
+				if (tabGroups[key].htmlElement.attr("id") == element.attr("id")) {
+					return tabGroups[key];
+				}
+			}
+		}
+		return null;
 	}
 	tabHandler = new TabHandler();
 
